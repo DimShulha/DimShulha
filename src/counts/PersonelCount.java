bf71cd19e2e0edae530653d203b71f25c3278b80
Lenovo <fdhdsfhhsdkkhdsjj@kffkk.dkkf>
2021-11-23 14:26:13 +0300

    Create new count for personel

---
+++ src/counts/PersonelCount.java
@@ -0,0 +1,19 @@
+package counts;
+
+import models.Personel;
+import view.PersonelView;
+
+import utils.Rounder;
+
+public class PersonelCount {
+
+    Personel model;
+    PersonelView views;
+
+    public void ClientCount(Personel model, PersonelView views){
+
+
+    }
+
+
+}
